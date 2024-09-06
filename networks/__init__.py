579c4731b6ae7468429b3ffe8b5367f5aa8f07bc
MSHZ <101371071+Mona-ShZeinoddin@users.noreply.github.com>
2024-09-06 09:50:40 +0100

    Update __init__.py

--- networks/__init__.py
+++ networks/__init__.py
@@ -4,7 +4,4 @@ from .pose_decoder import PoseDecoder
 from .pose_cnn import PoseCNN
 from .appearance_flow_decoder import TransformDecoder
 from .optical_flow_decoder import PositionDecoder
-# from .custom_DAM import Customised_DAM
-#from .custom_DAM_separatedLoRAinit import Customised_DAM
-from .custom_DAM_separatedVectorLoRAinit_32 import Customised_DAM
-# from .DAM import Customised_DAM
+from .DARES import Dares
