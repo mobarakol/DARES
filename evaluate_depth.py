de851c2a24c08ebc5c1f1d2cce51e61f4531f70d
MSHZ <101371071+Mona-ShZeinoddin@users.noreply.github.com>
2024-09-09 10:10:05 +0100

    Update evaluate_depth.py

--- evaluate_depth.py
+++ evaluate_depth.py
@@ -75,45 +75,21 @@ def evaluate(opt):
         print("-> Loading weights from {}".format(opt.load_weights_folder))
 
         filenames = readlines(os.path.join(splits_dir, opt.eval_split, "test_files.txt"))
-        # encoder_path = os.path.join(opt.load_weights_folder, "encoder.pth")
-        # decoder_path = os.path.join(opt.load_weights_folder, "depth.pth")
-        depth_model_path = os.path.join(opt.load_weights_folder, "depth_model.pth")
-
-        # encoder_dict = torch.load(encoder_path)
-        depth_model_dict = torch.load(depth_model_path)
-
-        # dataset = datasets.SCAREDRAWDataset(opt.data_path, filenames,
-        #                                    encoder_dict['height'], encoder_dict['width'],
-        #                                    [0], 4, is_train=False)
         dataset = datasets.SCAREDRAWDataset(opt.data_path, filenames,
                                            256,320,
                                            [0], 4, is_train=False)
         dataloader = DataLoader(dataset, 16, shuffle=False, num_workers=opt.num_workers,
                                 pin_memory=True, drop_last=False)
+        depth_model = networks.DARES()
 
-        # encoder = networks.ResnetEncoder(opt.num_layers, False)
-        # depth_decoder = networks.DepthDecoder(encoder.num_ch_enc, scales=range(4))
-        depth_model = networks.Customised_DAM()
-
-        # model_dict = encoder.state_dict()
         model_dict = depth_model.state_dict()
 
-        # encoder.load_state_dict({k: v for k, v in encoder_dict.items() if k in model_dict})
         depth_model.load_state_dict({k: v for k, v in depth_model_dict.items() if k in model_dict})
-        # depth_decoder.load_state_dict(torch.load(decoder_path))
-        # depth_model.load_state_dict(torch.load(depth_model_path))
-
-        # encoder.cuda()
-        # encoder.eval()
-        # depth_decoder.cuda()
-        # depth_decoder.eval()
         depth_model.cuda()
         depth_model.eval()
 
         pred_disps = []
 
-        # print("-> Computing predictions with size {}x{}".format(
-        #     encoder_dict['width'], encoder_dict['height']))
         print("-> Computing predictions with size {}x{}".format(
             320, 256))
 
@@ -125,7 +101,6 @@ def evaluate(opt):
                     # Post-processed results require each image to have two forward passes
                     input_color = torch.cat((input_color, torch.flip(input_color, [3])), 0)
 
-                # output = depth_decoder(encoder(input_color))
                 output = depth_model(input_color)
                 pred_disp, _ = disp_to_depth(output[("disp", 0)], opt.min_depth, opt.max_depth)
                 pred_disp = pred_disp.cpu()[:, 0].numpy()
